84fcdf5770a6f81acc83d3156e74994d6d807622
Luxana <luxana.oleszczuk@student.norwichuni.ac.uk>
2025-02-27 13:35:05 +0000

    jiggle bubble pop up function
    I started writhing the function that make bubbles pop up when the user aim and "shut" them.

---
+++ Assets/Scripts/CustomBubbles.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CustomBubbles : MonoBehaviour
+{   
+    [Tooltip("The object to be spawned")]
+    public GameObject prefab = null;
+    [Tooltip("The transform where the object is spawned")]
+    public Transform spawnTransform = null;
+    [Header("jiggle settings")]
+    [Tooltip("Time for bubbles to scale in and settle")]
+    public float jiggleDuration = 0.3f;
+    [Tooltip("How big is the bulle on its peak")]
+    public float overShootScale = 1.2f;
+    [Toolpit(Curve controlling the scaling motion from 0 to overshoot)]
+    public AnimationCurve jiggleCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+
+    public void Spawn(){
+        //create bubble
+        GameObject newObject = Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
+        //set random rotation
+        newObject.transform.rotation = Random.rotation;
+        //set randomised laund speed
+        float launchSpeed = Random.Range(0.5f, 1.5f);
+        //creates Vector3 to represent foward movement
+        Vector3 force =spawnTransform.forward * launchSpeed;
+        newObject.GetComponent<Rigidbody>().AddForce(force);
+        newObject.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+
+        //reset scale to 0 so it can pop in
+        newObject.transform.localScale = Vector3.zero;
+        //start the popup coroutine
+        StartCoroutine(Jiggle(newObject.transform));
+    }
+    private IEnumerator JiggleEffect(transform bubbleTransform) {
+        Vector3 startScale = Vector3 one;
+        float elapsedTime = 0f;
+
+        while (elapsedTime , jiggleDuration) {}
+    }
+}
