476545550ff059c1d69e5a657baddc5154057bf5
Luxana <luxana.oleszczuk@student.norwichuni.ac.uk>
2025-02-27 15:38:01 +0000

    Bubble function
    The bubble spawn, timing, and size fixed.

--- Assets/Scripts/CustomBubbles.cs
+++ Assets/Scripts/CustomBubbles.cs
@@ -11,10 +11,10 @@ public class CustomBubbles : MonoBehaviour
     // [Tooltip("Time for bubbles to scale in and settle")]
     public float jiggleDuration = 0.3f;
     // [Tooltip("How big is the bulle on its peak")]
-    public float overShootScale = 1.2f;
+    public float overShootScale = 1f;
    // [Toolpit(Curve controlling the scaling motion from 0 to overshoot)]
-    public AnimationCurve jiggleCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
-    Vector3 finalScale = new Vector3(10f, 10f, 10f);
+    public AnimationCurve jiggleCurve = AnimationCurve.EaseInOut(0.2f, 0.2f, 1f, 1f);
+    Vector3 finalScale = new Vector3(5f, 5f, 5f);
 
     public void Spawn(){
         //create bubble
@@ -22,7 +22,7 @@ public class CustomBubbles : MonoBehaviour
         //set random rotation
         newObject.transform.rotation = Random.rotation;
         //set randomised laund speed
-        float launchSpeed = Random.Range(0.5f, 1.5f);
+        float launchSpeed = Random.Range(0.7f, 1.5f);
         //creates Vector3 to represent foward movement
         Vector3 force =spawnTransform.forward * launchSpeed;
         newObject.GetComponent<Rigidbody>().AddForce(force);
